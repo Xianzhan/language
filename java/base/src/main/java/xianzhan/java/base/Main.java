f1b4ebfd0b852d4cbd0e349c2c2c3663065b406a
xianzhan <424447832@qq.com>
2020-09-12 15:52:31 +0800

    add: java

---
+++ java/base/src/main/java/xianzhan/java/base/Main.java
@@ -0,0 +1,11 @@
+package xianzhan.java.base;
+
+/**
+ * @author xianzhan
+ * @since 2020-09-12
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
